ee3cff616ce4cfdfb3cb86323653bd6c9e32792f
Nycolas da Guia Santos <50758229+Nycolas-Guia@users.noreply.github.com>
2023-09-29 15:00:58 -0300

    Explicação completa

--- PHP/Aula_3-print-echo/Teste.php
+++ PHP/Aula_3-print-echo/Teste.php
@@ -1,5 +1,19 @@
 <?php
-    print "Aula_3";
-    echo "<br>quebra de linha"
-    //MESMA COISA QUE A ULTIMA AULA
+    // O echo pode guardar mais de um elemento separado por vírgula, enquanto o print só pode concatenar    
+    print "Bom "."dia";
+    echo "Bom  ". "dia ", "amigos!";
+
+    /*Print:
+        Pode realizar operador ternário;
+        Pode ser guardado numa variável;
+        Pode ser retornado numa função.
+        
+      Echo:
+        Pode mostrar quantos elementos quiser com virgula;
+        É mais rápido que o print;
+        É o mais utilizado nas ocasiões em que executa as mesmas ações que o print.  
+    */
+    $p = ($idade <10) ? print ('menor') : print('maior'); //Exemplo de operador ternário
+//         Condição       True              False
+   
 ?>
